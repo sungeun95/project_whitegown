a1af0db56d4fb704148f62457c34bc4586d4451e
sungeun <cmqktk18@gmail.com>
2025-07-17 23:28:16 +0900

    2025/07/17

--- scripts/index.js
+++ scripts/index.js
@@ -125,6 +125,11 @@ if (swiper.isEnd) {
 /* 1행 스와이퍼 적용 */
 const bestSwiper = new Swiper('#gate_slide',{
     //옵션:값,
+    loop:true,
+    autoplay:{
+	delay:2000,
+    disableOnInteraction:false,
+},
 })
 
 
@@ -132,9 +137,25 @@ const value = new Swiper('#value_slide',{
     slidesPerView:1.5,
     centeredSlides:true,
     loop:true,
-    spaceBetween:100,
+    autoplay:{
+	delay:3000,
+    disableOnInteraction:false,
+    },
     navigation:{
         nextEl:'#value_slide .swiper-button-next',
         prevEl:'#value_slide .swiper-button-prev',
+    },
+    breakpoints: { //반응형 조건 속성
+        320: { 
+        slidesPerView: 3,
+        direction: 'vertical',
+        spaceBetween:10,
+        },
+        799: {
+        slidesPerView:1.5,
+        direction: 'horizontal',
+        spaceBetween:100,
+        },
     }
 })
+
